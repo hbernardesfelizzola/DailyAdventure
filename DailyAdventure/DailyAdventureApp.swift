979a2f71fa02882d35bb0736919d79d93a81331b
Henrique Felizzola <henrique.felizzola@poatek.com>
2026-02-09 00:36:56 -0300

    Initial Commit

---
+++ DailyAdventure/DailyAdventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyAdventureApp.swift
+//  DailyAdventure
+//
+//  Created by Henrique Bernardes on 09/02/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyAdventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
